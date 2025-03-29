df21d4f0252f811db9d55a1669cd3fcfdd014f22
nahK994 <nkskl6@gmail.com>
2025-03-30 01:53:50 +0600

    start vendor, rider and admin sections in frontend

---
+++ frontend/app/rider/page.tsx
@@ -0,0 +1,8 @@
+export default function Rider() {
+    return (
+      <div>
+        <h1>rider</h1>
+      </div>
+    );
+  }
+  
---
+++ frontend/app/vendor/page.tsx
@@ -0,0 +1,8 @@
+export default function Vendor() {
+    return (
+      <div>
+        <h1>vendor</h1>
+      </div>
+    );
+  }
+  
